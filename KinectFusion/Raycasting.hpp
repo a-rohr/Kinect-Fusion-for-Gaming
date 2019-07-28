1c20f66ad83eb687729ea886f5565c754b547cc8
faridyagubbayli <faridyagubbayli@gmail.com>
2019-07-28 18:50:10 +0200

    Changed Raycasting

--- KinectFusion/Raycasting.hpp
+++ KinectFusion/Raycasting.hpp
@@ -6,7 +6,7 @@
 
 #include "TsdfUtils.h"
 
-void raytraceImage(TsdfUtils::TsdfData tsdfData, Eigen::Matrix4f cameraPose, Eigen::Matrix3d cameraIntrisic,
+void castRays(TsdfUtils::TsdfData tsdfData, Eigen::Matrix4f cameraPose, Eigen::Matrix3d cameraIntrisic,
                       unsigned int resolutionWidth, unsigned int resolutionHeight,
                       const double stepSizeVoxel, const double epsilon,
                       cv::Mat& depthImage, cv::Mat& normalImage);
